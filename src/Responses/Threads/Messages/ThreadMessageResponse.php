cc81f00f9352c5e686f113ecb43a92311e7c6859
Anthony Dantard <anthony.dantard@gmail.com>
2024-10-03 15:16:25 +0200

    Fixed attachments and attachments[x].tools exception on ThreadMessageResponse.php

--- src/Responses/Threads/Messages/ThreadMessageResponse.php
+++ src/Responses/Threads/Messages/ThreadMessageResponse.php
@@ -12,12 +12,12 @@ use OpenAI\Responses\Meta\MetaInformation;
 use OpenAI\Testing\Responses\Concerns\Fakeable;
 
 /**
- * @implements ResponseContract<array{id: string, object: string, created_at: int, thread_id: string, role: string, content: array<int, array{type: 'text', text: array{value: string, annotations: array<int, array{type: 'file_citation', text: string, file_citation: array{file_id: string, quote?: string}, start_index: int, end_index: int}|array{type: 'file_path', text: string, file_path: array{file_id: string}, start_index: int, end_index: int}>}}|array{type: string, image_file: array{file_id: string, detail?: string}}|array{type: 'image_url', image_url: array{url: string, detail?: string}}>, assistant_id: ?string, run_id: ?string, attachments: array<int, array{file_id: string, tools: array<int, array{type: string}>}>, metadata: array<string, string>}>
+ * @implements ResponseContract<array{id: string, object: string, created_at: int, thread_id: string, role: string, content: array<int, array{type: 'text', text: array{value: string, annotations: array<int, array{type: 'file_citation', text: string, file_citation: array{file_id: string, quote?: string}, start_index: int, end_index: int}|array{type: 'file_path', text: string, file_path: array{file_id: string}, start_index: int, end_index: int}>}}|array{type: string, image_file: array{file_id: string, detail?: string}}|array{type: 'image_url', image_url: array{url: string, detail?: string}}>, assistant_id: ?string, run_id: ?string, attachments: ?array<int, array{file_id: ?string, tools: ?array<int, array{type: string}>}>, metadata: array<string, string>}>
  */
 final class ThreadMessageResponse implements ResponseContract, ResponseHasMetaInformationContract
 {
     /**
-     * @use ArrayAccessible<array{id: string, object: string, created_at: int, thread_id: string, role: string, content: array<int, array{type: 'text', text: array{value: string, annotations: array<int, array{type: 'file_citation', text: string, file_citation: array{file_id: string, quote?: string}, start_index: int, end_index: int}|array{type: 'file_path', text: string, file_path: array{file_id: string}, start_index: int, end_index: int}>}}|array{type: string, image_file: array{file_id: string, detail?: string}}|array{type: 'image_url', image_url: array{url: string, detail?: string}}>, assistant_id: ?string, run_id: ?string, attachments: array<int, array{file_id: string, tools: array<int, array{type: string}>}>, metadata: array<string, string>}>
+     * @use ArrayAccessible<array{id: string, object: string, created_at: int, thread_id: string, role: string, content: array<int, array{type: 'text', text: array{value: string, annotations: array<int, array{type: 'file_citation', text: string, file_citation: array{file_id: string, quote?: string}, start_index: int, end_index: int}|array{type: 'file_path', text: string, file_path: array{file_id: string}, start_index: int, end_index: int}>}}|array{type: string, image_file: array{file_id: string, detail?: string}}|array{type: 'image_url', image_url: array{url: string, detail?: string}}>, assistant_id: ?string, run_id: ?string, attachments: ?array<int, array{file_id: ?string, tools: ?array<int, array{type: string}>}>, metadata: array<string, string>}>
      */
     use ArrayAccessible;
 
@@ -25,33 +25,33 @@ final class ThreadMessageResponse implements ResponseContract, ResponseHasMetaIn
     use HasMetaInformation;
 
     /**
-     * @param  array<int, ThreadMessageResponseContentTextObject|ThreadMessageResponseContentImageFileObject|ThreadMessageResponseContentImageUrlObject>  $content
-     * @param  array<int, ThreadMessageResponseAttachment>  $attachments
-     * @param  array<string, string>  $metadata
+     * @param array<int, ThreadMessageResponseContentTextObject|ThreadMessageResponseContentImageFileObject|ThreadMessageResponseContentImageUrlObject> $content
+     * @param array<int, ThreadMessageResponseAttachment> $attachments
+     * @param array<string, string> $metadata
      */
     private function __construct(
-        public string $id,
-        public string $object,
-        public int $createdAt,
-        public string $threadId,
-        public string $role,
-        public array $content,
-        public ?string $assistantId,
-        public ?string $runId,
-        public array $attachments,
-        public array $metadata,
+        public string                    $id,
+        public string                    $object,
+        public int                       $createdAt,
+        public string                    $threadId,
+        public string                    $role,
+        public array                     $content,
+        public ?string                   $assistantId,
+        public ?string                   $runId,
+        public ?array                    $attachments,
+        public array                     $metadata,
         private readonly MetaInformation $meta,
     ) {}
 
     /**
      * Acts as static factory, and returns a new Response instance.
      *
-     * @param  array{id: string, object: string, created_at: int, thread_id: string, role: string, content: array<int, array{type: 'image_url', image_url: array{url: string, detail?: string}}|array{type: 'image_file', image_file: array{file_id: string, detail?: string}}|array{type: 'text', text: array{value: string, annotations: array<int, array{type: 'file_citation', text: string, file_citation: array{file_id: string, quote?: string}, start_index: int, end_index: int}|array{type: 'file_path', text: string, file_path: array{file_id: string}, start_index: int, end_index: int}>}}>, assistant_id: ?string, run_id: ?string, attachments: array<int, array{file_id: string, tools: array<int, array{type: 'file_search'}|array{type: 'code_interpreter'}>}>, metadata: array<string, string>}  $attributes
+     * @param array{id: string, object: string, created_at: int, thread_id: string, role: string, content: array<int, array{type: 'image_url', image_url: array{url: string, detail?: string}}|array{type: 'image_file', image_file: array{file_id: string, detail?: string}}|array{type: 'text', text: array{value: string, annotations: array<int, array{type: 'file_citation', text: string, file_citation: array{file_id: string, quote?: string}, start_index: int, end_index: int}|array{type: 'file_path', text: string, file_path: array{file_id: string}, start_index: int, end_index: int}>}}>, assistant_id: ?string, run_id: ?string, attachments: ?array<int, array{file_id: ?string, tools: ?array<int, array{type: 'file_search'}|array{type: 'code_interpreter'}>}>, metadata: array<string, string>} $attributes
      */
     public static function from(array $attributes, MetaInformation $meta): self
     {
         $content = array_map(
-            fn (array $content): ThreadMessageResponseContentTextObject|ThreadMessageResponseContentImageFileObject|ThreadMessageResponseContentImageUrlObject => match ($content['type']) {
+            fn(array $content): ThreadMessageResponseContentTextObject|ThreadMessageResponseContentImageFileObject|ThreadMessageResponseContentImageUrlObject => match ($content['type']) {
                 'text' => ThreadMessageResponseContentTextObject::from($content),
                 'image_file' => ThreadMessageResponseContentImageFileObject::from($content),
                 'image_url' => ThreadMessageResponseContentImageUrlObject::from($content),
@@ -59,10 +59,10 @@ final class ThreadMessageResponse implements ResponseContract, ResponseHasMetaIn
             $attributes['content'],
         );
 
-        $attachments = array_map(
-            fn (array $attachment): ThreadMessageResponseAttachment => ThreadMessageResponseAttachment::from($attachment),
+        $attachments = is_array($attributes['attachments']) ? array_map(
+            fn(array $attachment): ThreadMessageResponseAttachment => ThreadMessageResponseAttachment::from($attachment),
             $attributes['attachments']
-        );
+        ) : null;
 
         return new self(
             $attributes['id'],
@@ -91,13 +91,13 @@ final class ThreadMessageResponse implements ResponseContract, ResponseHasMetaIn
             'thread_id' => $this->threadId,
             'role' => $this->role,
             'content' => array_map(
-                fn (ThreadMessageResponseContentImageFileObject|ThreadMessageResponseContentTextObject|ThreadMessageResponseContentImageUrlObject $content): array => $content->toArray(),
+                fn(ThreadMessageResponseContentImageFileObject|ThreadMessageResponseContentTextObject|ThreadMessageResponseContentImageUrlObject $content): array => $content->toArray(),
                 $this->content,
             ),
-            'attachments' => array_map(
+            'attachments' => is_array($this->attachments) ? array_map(
                 fn (ThreadMessageResponseAttachment $attachment): array => $attachment->toArray(),
                 $this->attachments
-            ),
+            ) : null,
             'assistant_id' => $this->assistantId,
             'run_id' => $this->runId,
             'metadata' => $this->metadata,
--- src/Responses/Threads/Messages/ThreadMessageResponseAttachment.php
+++ src/Responses/Threads/Messages/ThreadMessageResponseAttachment.php
@@ -9,40 +9,42 @@ use OpenAI\Responses\Concerns\ArrayAccessible;
 use OpenAI\Testing\Responses\Concerns\Fakeable;
 
 /**
- * @implements ResponseContract<array{file_id: string, tools: array<int, array{type: string}>}>
+ * @implements ResponseContract<array{file_id: ?string, tools: ?array<int, array{type: string}>}>
  */
 final class ThreadMessageResponseAttachment implements ResponseContract
 {
     /**
-     * @use ArrayAccessible<array{file_id: string, tools: array<int, array{type: string}>}>
+     * @use ArrayAccessible<array{file_id: ?string, tools: ?array<int, array{type: string}>}>
      */
     use ArrayAccessible;
 
     use Fakeable;
 
     /**
-     * @param  array<int,ThreadMessageResponseAttachmentFileSearchTool|ThreadMessageResponseAttachmentCodeInterpreterTool>  $tools
+     * @param array<int,ThreadMessageResponseAttachmentFileSearchTool|ThreadMessageResponseAttachmentCodeInterpreterTool> $tools
      */
     private function __construct(
-        public string $fileId,
-        public array $tools,
+        public ?string $fileId,
+        public ?array  $tools,
     ) {}
 
     /**
      * Acts as static factory, and returns a new Response instance.
      *
-     * @param  array{file_id: string, tools: array<int, array{type: 'file_search'}|array{type: 'code_interpreter'}>}  $attributes
+     * @param array{file_id: ?string, tools: ?array<int, array{type: 'file_search'}|array{type: 'code_interpreter'}>} $attributes
      */
     public static function from(array $attributes): self
     {
-        $tools = array_map(fn (array $tool): ThreadMessageResponseAttachmentFileSearchTool|ThreadMessageResponseAttachmentCodeInterpreterTool => match ($tool['type']) {
-            'file_search' => ThreadMessageResponseAttachmentFileSearchTool::from($tool),
-            'code_interpreter' => ThreadMessageResponseAttachmentCodeInterpreterTool::from($tool),
-        }, $attributes['tools']);
+        $tools = is_array($attributes['tools']) ? array_map(
+            fn(array $tool): ThreadMessageResponseAttachmentFileSearchTool|ThreadMessageResponseAttachmentCodeInterpreterTool => match ($tool['type']) {
+                'file_search' => ThreadMessageResponseAttachmentFileSearchTool::from($tool),
+                'code_interpreter' => ThreadMessageResponseAttachmentCodeInterpreterTool::from($tool),
+            }, $attributes['tools']
+        ) : null;
 
         return new self(
-            $attributes['file_id'],
-            $tools,
+            $attributes['file_id'] ?? null,
+            $tools ?? null,
         );
     }
 
@@ -52,11 +54,11 @@ final class ThreadMessageResponseAttachment implements ResponseContract
     public function toArray(): array
     {
         return [
-            'file_id' => $this->fileId,
-            'tools' => array_map(
-                fn (ThreadMessageResponseAttachmentCodeInterpreterTool|ThreadMessageResponseAttachmentFileSearchTool $tool): array => $tool->toArray(),
+            'file_id' => $this->fileId ?? null,
+            'tools' => is_array($this->tools) ? array_map(
+                fn(ThreadMessageResponseAttachmentCodeInterpreterTool|ThreadMessageResponseAttachmentFileSearchTool $tool): array => $tool->toArray(),
                 $this->tools,
-            ),
+            ) : null,
         ];
     }
 }
